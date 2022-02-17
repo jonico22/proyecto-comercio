c6388c3cdd69f0bba557b216ac37fc370c2d6c31
Jose Luis <joseyananicolas@gmail.com>
2022-02-16 23:54:59 -0500

    cors

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const cors = require('cors')
 const PORT = 8080;
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(cors())
 const routerProductos = require('./api/routeProduct');
 app.use('/api/productos', routerProductos);
 
